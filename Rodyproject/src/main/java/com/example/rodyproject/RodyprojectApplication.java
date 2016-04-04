d028b0ced1aa97aa7e95e26932458d521a5261c4
Pavel Brodianskyi <brodianskyi@gmail.com>
2016-04-04 22:50:14 +0200

    My First Commit

---
+++ Rodyproject/src/main/java/com/example/rodyproject/RodyprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.rodyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodyprojectApplication.class, args);
+	}
+}
